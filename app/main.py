e994b576cc988c883a19ab801abb2a89c1663357
Swapnil Daxini <Swapnil Daxini>
2017-03-04 15:41:33 -0800

    removed food path

--- app/main.py
+++ app/main.py
@@ -91,7 +91,7 @@ def move():
 	food = data['food']
 	dirsCanGo = directionsCanGo( parsedMapData, ourSnake, board_height, board_width, otherSnakes, food)
 	
-	if(data['you']['health_points'] < 65):
+	if(ourSnake['health_points'] < 65):
 		dirWantGo = findFood(data, ourSnake)
 		print(dirWantGo)
 		
